230f793e96b6b8ae7eae213de82f67c7c450946b
zhe.ma <mailMazhe@163.com>
2019-11-12 00:19:21 +0800

    Add self ping.

--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func pingServer() error {
+	for i := 0; i < 2; i++ {
+		rsp, err := http.Get("http://localhost:8080/version")
+		if err == nil && rsp.StatusCode == 200 {
+			return nil
+		}
+
+		time.Sleep(time.Second)
+	}
+
+	return errors.New("Cannot connect to the server.")
+}
+
+func GetVersion(c *gin.Context) {
+	c.String(http.StatusOK, "version1.0")
+}
 
 func main() {
 	server := gin.Default()
-	server.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World!")
-	})
+	server.GET("/version", GetVersion)
+
+	go func() {
+		err := pingServer()
+		if err != nil {
+			log.Fatal("Failed to ping server. The server has not response. Error: ", err)
+			return
+		}
+		log.Println("Ping server successfully!")
+	}()
 
 	server.Run()
 }
